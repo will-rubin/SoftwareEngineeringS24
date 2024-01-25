665e90bb81b835d424c73b6f5e86b1a197c05d48
CyrusMika-NewPaltz <157427632+CyrusMika-NewPaltz@users.noreply.github.com>
2024-01-25 12:40:26 -0500

    Update featureTest.java

--- featureTest.java
+++ featureTest.java
@@ -2,7 +2,7 @@ public class featureTest
 {
     public static void main(String[] args)
     {
-        System.out.print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.print("Hello World");
     }
 
 }
