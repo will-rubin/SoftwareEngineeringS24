865134bd0988d61ac332f24c9f0a59224895472c
CyrusMika-NewPaltz <157427632+CyrusMika-NewPaltz@users.noreply.github.com>
2024-02-12 15:07:45 -0500

    Update TestClientUser.java

--- test/Scratch/TestClientUser.java
+++ test/Scratch/TestClientUser.java
@@ -4,4 +4,12 @@ import mockito.Mockito.any;
 
 public class TestClientUser
   {
+    public void testUser() throws Exception
+    {
+      User mockUser = Mockito.mock(User.class);
+      when (mockUser.isEmpty())
+        {
+          .thenThrow(new RunTimeException("User Error"));
+        }
+    }
   }
