63ff060f1c1a35874d994029d502c329b3d20aea
Will Rubin <123343629+will-rubin@users.noreply.github.com>
2024-02-12 13:29:31 -0500

    refactor as project

--- test/Scratch/Test.java
+++ test/Scratch/Test.java
@@ -1,7 +1,14 @@
 
 public class Test {
+
     public static void main(String[] args) {
         System.out.print("Testing");
     }
+
+    @Test
+    public void printMyTest() {
+        System.out.println("My test");
+    }
+
 
 }
