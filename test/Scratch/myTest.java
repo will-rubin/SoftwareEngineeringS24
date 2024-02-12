9c4ac1ba4c9efd769c44c9cbeab57043316291cb
Will Rubin <123343629+will-rubin@users.noreply.github.com>
2024-02-12 13:42:00 -0500

    added JUnit

---
+++ test/Scratch/myTest.java
@@ -0,0 +1,19 @@
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+
+public class myTest {
+
+    @Test
+    public static void main(String[] args) {
+        System.out.print("Testing");
+    }
+
+    @Test
+    public void printMyTest() throws Exception {
+        System.out.println("My test");
+    }
+
+
+}
