47a92ade16a36fd485a314c6acb658c3966fd903
alexwyant <130941024+alexwyant@users.noreply.github.com>
2024-02-12 16:59:25 -0500

    Create DataStore.java (#17)

---
+++ src/DataStore.java
@@ -0,0 +1,14 @@
+public class InMemoryDataStore implements DataStore{
+    public void processData(InputConfig inputConfig, OutputConfig outputConfig) {
+        List<Integer> inputData = (List<Integer>) inputConfig.getIterator().next();
+      //Read the list of integers
+        for (Integer inputValue : inputData) {
+            String processedResult = processItem(inputValue); 
+          //Read the string 
+            outputConfig.write(processedResult);
+        }
+    }
+    private String processItem(Integer inputValue) {
+        return "Processed: " + inputValue;
+    }
+}
