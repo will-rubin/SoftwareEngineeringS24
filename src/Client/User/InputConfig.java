674a33ed48d4991f9e91cfab29aafd13b0423bde
alexwyant <130941024+alexwyant@users.noreply.github.com>
2024-02-12 16:44:30 -0500

    Update InputConfig.java

--- src/Client/User/InputConfig.java
+++ src/Client/User/InputConfig.java
@@ -1,4 +1,14 @@
 public interface InputConfig {
+    public class InMemoryInputConfig<T> implements InputConfig<T> {
+        private final List<T> data;
+        public InMemoryInputConfig(List<T> data) {
+            this.data = Collections.unmodifiableList(data); 
+    }
+    @Override
+    public Iterator<T> getIterator() {
+        return data.iterator();
+    }
+}
 
     
 } 
