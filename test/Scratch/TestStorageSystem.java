051ec1b98758cb96729fab062b2b905ffdc9246e
CyrusMika-NewPaltz <157427632+CyrusMika-NewPaltz@users.noreply.github.com>
2024-02-12 14:56:07 -0500

    Update TestStorageSystem.java

--- test/Scratch/TestStorageSystem.java
+++ test/Scratch/TestStorageSystem.java
@@ -4,4 +4,15 @@ import mockito.Mockito.any;
 
 public class TestStorageSystem
   {
+    public void testAddResult() throws Exception 
+    {
+      int result = new StorageSystem.addResult(2,3);
+      assert result == 5;
+    }
+    public void testRead()
+    {
+      integer read = new StorageSystem.read(1234567890);
+      assert read.isInteger();
+    }
+    
   }
