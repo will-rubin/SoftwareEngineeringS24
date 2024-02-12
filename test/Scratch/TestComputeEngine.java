fc5ff99659c02db403488a5ccd2a60b6a8c7c230
Will Rubin <123343629+will-rubin@users.noreply.github.com>
2024-02-12 16:26:50 -0500

    Project creation (#11)
    * created initial folders
    
    * refactor into Client/Server
    
    * getting ready to add API comments
    
    * updated comments and readme
    
    * Update README.md
    
    * some simple API comments to start with
    
    * Update User.java
    
    * Update ComputeEngine.java
    
    * Update ComputeEngine.java
    
    * Update ComputeEngine.java
    
    * Update User.java
    
    * Update ComputeEngine.java
    
    * Developing some APIs
    
    * Update StorageSystem.java
    
    * Update StorageSystem.java
    
    * Reworked to add interfaces and protoypes
    
    * Added prototype and interface
    
    * Update User.java
    
    * Update StorageSystem.java
    
    * Update EngineController.java
    
    * Update StorageSystem.java
    
    * Update User.java
    
    * Update StorageSystem.java
    
    * Added interface and prototype
    
    * fixed typo
    
    * refactor into src/test
    
    * Create scratch folder
    
    * refactor as project
    
    * added JUnit
    
    * added API files and requests we were missing
    
    * fixing for checkstyle
    
    * fixing for checkstyle
    
    * Create TestComputeEngine.java
    
    * fix checkstyle
    
    * fix checkstyle
    
    * Create TestStorageSystem.java (#12)
    
    * Create TestClientUser.java
    
    * Update TestStorageSystem.java (#13)
    
    * Update TestComputeEngine.java
    
    * Update TestClientUser.java
    
    * Create IntegreationTest.java
    
    * Update TestClientUser.java
    
    * Update TestComputeEngine.java
    
    * Update TestStorageSystem.java
    
    * Update IntegreationTest.java
    
    ---------
    
    Co-authored-by: CyrusMika-NewPaltz <157427632+CyrusMika-NewPaltz@users.noreply.github.com>
    Co-authored-by: alexwyant <130941024+alexwyant@users.noreply.github.com>

--- test/Scratch/TestComputeEngine.java
+++ test/Scratch/TestComputeEngine.java
@@ -3,4 +3,8 @@ import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
 
 public class TestComputeEngine {
+  public void testCompute() {
+    String result = ComputeEngine.compute(8675309);
+    assert result.isString();
+  }
 }
--- test/Scratch/TestStorageSystem.java
+++ test/Scratch/TestStorageSystem.java
@@ -1,7 +1,15 @@
-import org.mockito.Mockito;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.any;
+import mockito.Mockito;
+import mockito.Mockito.when;
+import mockito.Mockito.any;
 
 public class TestStorageSystem {
-
-}
+    public void testAddResult() throws Exception {
+      int result = new StorageSystem.addResult(2,3);
+      assert result == 5;
+    }
+    public void testRead() {
+      integer read = new StorageSystem.read(1234567890);
+      assert read.isInteger();
+    }
+    
+  }
