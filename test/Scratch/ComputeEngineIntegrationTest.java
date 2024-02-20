27598f69378fa14b4de760cbe29b57649db0b115
CyrusMika-NewPaltz <157427632+CyrusMika-NewPaltz@users.noreply.github.com>
2024-02-20 13:07:11 -0500

    Update ComputeEngineIntegrationTest.java

--- test/Scratch/ComputeEngineIntegrationTest.java
+++ test/Scratch/ComputeEngineIntegrationTest.java
@@ -1,6 +1,3 @@
 public class ComputeEngineIntegrationTest{
-    String result = "";
-    InputConfig getInputConfig(result);
-    OutputConfig theOutput = getOutputConfig(result);
-    return theOutput.equals(result);
+
   }
