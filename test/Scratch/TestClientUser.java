fc6eb53fc6cb4df58b3305c8afef61d9db17bcef
Will Rubin <123343629+will-rubin@users.noreply.github.com>
2024-02-20 21:00:03 -0500

    fixing typo

--- test/Scratch/TestClientUser.java
+++ test/Scratch/TestClientUser.java
@@ -1,12 +1,12 @@
-import mockito.Mockito;
-import mockito.Mockito.when;
-import mockito.Mockito.any;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 
-public class TestClientUser{
+public class TestClientUser {
     /*public void testUser() throws Exception{
       User mockUser = Mockito.mock(User.class);
       when (mockUser.isEmpty()) {
           .thenThrow(new RunTimeException("User Error"));
         }
     }*/
-  }
+}
