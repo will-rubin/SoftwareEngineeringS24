3c9057f0ea8490d18ab4d6a301733bc07aa1548c
CyrusMika-NewPaltz <157427632+CyrusMika-NewPaltz@users.noreply.github.com>
2024-02-22 13:53:33 -0500

    Update ComputeEngine.java

--- src/Server/ComputeEngine/ComputeEngine.java
+++ src/Server/ComputeEngine/ComputeEngine.java
@@ -3,7 +3,34 @@ public interface ComputeEngine {
     /* What the compute engine needs to be able to do
      * This component will do the actual calculation and will not be the "user"
      */
-    String compute(int value);
+      String compute(int value);
+      Integer number = Integer.parseInt(User.input);
+      //do the calculation
+      int empty=0;
+      String result="";
+      System.out.println(28/6);
+      for (int a=1; a<number; a++) {
+          if (number%a == 0) {
+              //System.out.println(theThing);
+              //System.out.println(true);
+              //empty+=theThing;
+              System.out.println("Current value"+a);
+              empty+=a;
+          }
+        
+      }
+      if (empty==number) {
+        result="perfect";
+      }
+      else if (empty<number) {
+        result="deficient";
+      }
+      else {
+        result="abundant";
+      }
+      //return the result in a specified fashion
+      System.out.println(result);
+      String compute(String result);
 }
 
 
