aecfb2ae0854232c8c1ce3cce8f411ca6f866e5c
Will Rubin <123343629+will-rubin@users.noreply.github.com>
2024-02-22 23:00:58 -0500

    added suppress warnings

--- test/TestStorageSystem.java
+++ test/TestStorageSystem.java
@@ -2,11 +2,6 @@ import server.InputConfig;
 import server.OutputConfig;
 import server.StorageSystem;
 import server.WriteResult;
-<<<<<<< HEAD
-=======
-import server.OutputConfig.InMemoryOutputConfig;
->>>>>>> a88d7c0 (added way more tests)
-
 public class TestStorageSystem implements StorageSystem {
     @SuppressWarnings({ "rawtypes", "unchecked" })
 	public Iterable<Integer> read(InputConfig input) {
