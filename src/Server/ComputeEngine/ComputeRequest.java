88a42c48abfc0f1be42ba1181be5e16cc324c1dd
CyrusMika-NewPaltz <157427632+CyrusMika-NewPaltz@users.noreply.github.com>
2024-02-22 23:16:14 -0500

    Delete src/Server/ComputeEngine directory

--- src/Server/ComputeEngine/ComputeRequest.java
+++
@@ -1,9 +0,0 @@
-
-import src.Client.User.InputConfig;
-import src.Client.User.OutputConfig;
-
-public interface ComputeRequest {
-    InputConfig getInputConfig();
-    OutputConfig getOutputConfig();
-    char getDelimiter();
-}
