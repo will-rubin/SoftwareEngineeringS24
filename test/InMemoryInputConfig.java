e548dd8009a87d86f36d9251512bfb27d9ef6aba
Will Rubin <123343629+will-rubin@users.noreply.github.com>
2024-02-22 22:32:09 -0500

    fixed typo

--- test/InMemoryInputConfig.java
+++ test/InMemoryInputConfig.java
@@ -8,8 +8,5 @@ public class InMemoryInputConfig<T> implements InputConfig {
     private final List<T> data;
     public InMemoryInputConfig(List<T> data) {
         this.data = Collections.unmodifiableList(data); 
-}
-@Override
-public Iterator<T> getIterator() {
-    return data.iterator();
+    }
 }
