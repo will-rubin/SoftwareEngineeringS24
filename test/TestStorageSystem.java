de5fc728d5402961a8f4bfff38aa91d9aa220a43
Will Rubin <123343629+will-rubin@users.noreply.github.com>
2024-02-25 18:17:16 -0500

    fix typo

--- test/TestStorageSystem.java
+++ test/TestStorageSystem.java
@@ -2,10 +2,6 @@ import server.InputConfig;
 import server.OutputConfig;
 import server.StorageSystem;
 import server.WriteResult;
-<<<<<<< HEAD
-=======
-
->>>>>>> 15530281fd77d53f4d564e3bfe520854f3d8d2a7
 public class TestStorageSystem implements StorageSystem {
     @SuppressWarnings({ "rawtypes", "unchecked" })
 	public Iterable<Integer> read(InputConfig input) {
